7a98e69cd60a620f1e994af97f7ccef42358e616
Gyutae <gyutae07@gmail.com>
2021-08-21 20:37:21 +0900

    feat : Finite State Machine 로직 추가 - reference git : https://github.com/dubit/unity-fsm

---
+++ Union/Assets/Scripts/FiniteStateMachine/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Union.Services.FiniteStateMachine
+{
+    public class Controller
+    {
+		public event Action OnRan;
+		public event Action OnEntered;
+		public event Action OnExited;
+
+		public void Run()
+        {
+			if (OnRan != null) OnRan();
+        }
+
+		public void Enter()
+		{
+			if (OnEntered != null) OnEntered();
+		}
+
+		public void Exit()
+		{
+			if (OnExited != null) OnExited();
+		}
+	}
+}
---
+++ Union/Assets/Scripts/FiniteStateMachine/Machine.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+/*
+ * reference git : https://github.com/dubit/unity-fsm
+ */
+namespace Union.Services.FiniteStateMachine
+{ 
+    public class Machine<TState> where TState : IComparable
+    {
+        public TState CurrentState { get; private set; }
+
+        private Transition<TState> _currentTransition;
+
+        private readonly Dictionary<TState, Dictionary<string, Transition<TState>>> _transitions;
+        private readonly Dictionary<TState, Controller> _controller;
+
+        private event Action<TState, TState> _onStateChange;
+
+        private bool _isInitialisingState;
+
+        public Machine(params TState[] states)
+        {
+            if (states.Length < 1)
+            {
+                throw new ArgumentException("A FiniteStateMachine needs at least 1 state", "states");
+            }
+
+            this._transitions = new Dictionary<TState, Dictionary<string, Transition<TState>>>();
+            this._controller = new Dictionary<TState, Controller>();
+
+            foreach (var value in states)
+            {
+                this._transitions.Add(value, new Dictionary<string, Transition<TState>>());
+                this._controller.Add(value, new Controller());
+            }
+        }
+
+        public static Machine<TState> FromEnum()
+        {
+            if (typeof(Enum).IsAssignableFrom(typeof(TState)) == false)
+            {
+                throw new Exception("Can't create finite");
+            }
+
+            List<TState> states = new List<TState>();
+            foreach (TState value in Enum.GetValues(typeof(TState)))
+            {
+                states.Add(value);
+            }
+
+            return new Machine<TState>(states.ToArray());
+        }
+
+        public Machine<TState> AddTransition(TState fromState, TState toState, string command, Transition<TState> transition = null)
+        {
+            if (this._transitions.ContainsKey(fromState) == false)
+            {
+                throw new ArgumentException("unknown state", "from");
+            }
+
+            if (this._transitions.ContainsKey(toState) == false)
+            {
+                throw new ArgumentException("unknown state", "to");
+            }
+
+            this._transitions[fromState][command] = transition ?? new DefaultStateTransition<TState>(fromState, toState);
+
+            return this;
+        }
+
+        public Machine<TState> AddTransition(TState fromState, TState toState, string command, Func<bool> condition)
+        {
+            if (this._transitions.ContainsKey(fromState) == false)
+            {
+                throw new ArgumentException("unknown state", "from");
+            }
+
+            if (this._transitions.ContainsKey(toState) == false)
+            {
+                throw new ArgumentException("unknown state", "to");
+            }
+
+            this._transitions[fromState][command] = new DefaultStateTransition<TState>(fromState, toState, condition);
+
+            return this;
+        }
+
+        public Machine<TState> OnRun(TState state, Action handler)
+        {
+            if (this._transitions.ContainsKey(state) == false)
+            {
+                throw new ArgumentException("unknown state", "state");
+            }
+
+            if (handler == null)
+            {
+                throw new ArgumentNullException("handler");
+            }
+
+            this._controller[state].OnRan += handler;
+
+            return this;
+        }
+
+        public Machine<TState> OnEnter(TState state, Action handler)
+        {
+            if (this._transitions.ContainsKey(state) == false)
+            {
+                throw new ArgumentException("unknown state", "state");
+            }
+
+            if (handler == null)
+            {
+                throw new ArgumentNullException("handler");
+            }
+
+            this._controller[state].OnEntered += handler;
+
+            return this;
+        }
+
+        public Machine<TState> OnExit(TState state, Action handler)
+        {
+            if (this._transitions.ContainsKey(state) == false)
+            {
+                throw new ArgumentException("unknown state", "state");
+            }
+
+            if (handler == null)
+            {
+                throw new ArgumentNullException("handler");
+            }
+
+            this._controller[state].OnExited += handler;
+
+            return this;
+        }
+
+        public Machine<TState> OnChange(Action<TState, TState> handler)
+        {
+            if (handler == null)
+            {
+                throw new ArgumentNullException("handler");
+            }
+
+            this._onStateChange += handler;
+
+            return this;
+        }
+
+        public Machine<TState> OnChange(TState fromState, TState toState, Action handler)
+        {
+            if (this._transitions.ContainsKey(fromState) == false)
+            {
+                throw new ArgumentException("unknown state", "from");
+            }
+
+            if (this._transitions.ContainsKey(toState) == false)
+            {
+                throw new ArgumentException("unknown state", "to");
+            }
+
+            if (handler == null)
+            {
+                throw new ArgumentNullException("handler");
+            }
+
+            this._onStateChange += (fromState, toState) =>
+            {
+                if (fromState.Equals(fromState) && toState.Equals(toState))
+                {
+                    handler();
+                }
+            };
+
+            return this;
+        }
+
+        public void SetState(TState state)
+        {
+            if (this._transitions.ContainsKey(state) == false)
+            {
+                throw new ArgumentException("unknown state", "state");
+            }
+
+            this.CurrentState = state;
+        }
+
+        public void Run()
+        {
+            if (this._transitions.ContainsKey(this.CurrentState) == false)
+            {
+                throw new ArgumentException("unknown state", "state");
+            }
+
+            this._controller[this.CurrentState].Run();
+        }
+
+        public void IssueCommand(string command)
+        {
+            if (this._currentTransition != null)
+                return;
+
+            var transitionsForCurrentState = this._transitions[this.CurrentState];
+            if (transitionsForCurrentState.ContainsKey(command) == true)
+            {
+                if (this._isInitialisingState == true)
+                {
+                    Debug.LogWarning("Do not call IssueCommand from OnStateChange and OnStateEnter handlers");
+                    return;
+                }
+
+                var transition = transitionsForCurrentState[command];
+                if (transition.IsConditionFunctionExist() == true)
+                {
+                    transition.OnComplete += HandleTransitionComplete;
+                    this._currentTransition = transition;
+                    this._controller[this.CurrentState].Exit();
+                    transition.Begin();
+                }
+            }
+        }
+
+        public void HandleTransitionComplete()
+        {
+            this._currentTransition.OnComplete -= HandleTransitionComplete;
+
+            var previousState = this.CurrentState;
+            this.CurrentState = _currentTransition.ToState;
+
+            this._currentTransition = null;
+            this._isInitialisingState = true;
+
+            if (this._onStateChange != null)
+            {
+                this._onStateChange(previousState, this.CurrentState);
+            }
+
+            this._controller[this.CurrentState].Enter();
+            this._isInitialisingState = false;
+        }
+    }
+}
